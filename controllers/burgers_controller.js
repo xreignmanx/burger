ca4be4e1023e6b4e602f1e200955fdb8f7e1a6b1
xreignmanx <rtballentine@yahoo.com>
2018-12-23 18:58:54 -0500

    index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,35 +2,35 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+router.post("/api/burgers", function(req, res) {
+  burger.create(["name", "devoured"], [req.body.name, req.body.sleepy], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update(
+  burger.update(
     {
-      sleepy: req.body.sleepy
+      devoured: req.body.sleepy
     },
     condition,
     function(result) {
