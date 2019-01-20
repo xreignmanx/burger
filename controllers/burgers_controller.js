dbae824e11daaf9d1f976ea9359431140b13ce90
xreignmanx <rtballentine@yahoo.com>
2019-01-20 13:32:32 -0500

    settings for Heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "devoured"], [req.body.name, req.body.sleepy], function(result) {
+  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -30,7 +30,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   burger.update(
     {
-      devoured: req.body.sleepy
+      devoured: req.body.devoured
     },
     condition,
     function(result) {
