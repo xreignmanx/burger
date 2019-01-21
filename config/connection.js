65311412fb01e4f6bc5b2c2ed7133325eb7179ca
xreignmanx <rtballentine@yahoo.com>
2019-01-21 14:22:32 -0500

    port config

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  // DB is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // DB is local on localhost
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
